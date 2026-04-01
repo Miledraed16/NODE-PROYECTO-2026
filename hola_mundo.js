11cc003af069580a233a9e25ea5ab44cbb3c3862
miledraed16e <miledraed16@gmail.com>
2026-04-01 19:41:23 -0300

    node

---
+++ hola_mundo.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 10; i++) {
+console.log("Hola Mundo");
+}
+var i= 'Miled';
+console.log(i);
+const pi = 3.1416;
+console.log(pi);
+const lista = [1, 2, 3, 4, 5];
+lista.push(6);
+console.log(lista);
---
+++ hola_mundo_2.js
@@ -0,0 +1,6 @@
+const lista = [112, 17, 2, 4, 7];
+console.log('lista original:');
+console.log(lista);
+
+console.log('Lista Ordenada:');
+console.log(lista.sort((a, b) => a - b));
